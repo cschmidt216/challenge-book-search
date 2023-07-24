a404ec710150a300c794f827caef209cf0cea6f3
cschmidt216 <iamdrainy@gmail.com>
2023-07-24 01:59:31 -0700

    deploying

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider } from '@apollo/client';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-// Create an ApolloClient instance
-const client = new ApolloClient({
-  uri: 'https://serene-dusk-48581-595086ab6dc3.herokuapp.com/graphql', 
-  cache: new InMemoryCache(),
-});
-
 ReactDOM.render(
   <React.StrictMode>
-    {/* Wrap the App component with ApolloProvider and pass the client as a prop */}
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
