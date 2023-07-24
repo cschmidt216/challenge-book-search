52e06976886d7e7f3eae9d51ca3c41a3b7c4c69a
cschmidt216 <iamdrainy@gmail.com>
2023-07-24 15:08:00 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/book', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  dbName: 'book'
 });
 
 module.exports = mongoose.connection;
