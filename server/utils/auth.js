ae2ff36f04cff79125a7e92c4ee6f14d28d388e4
cschmidt216 <iamdrainy@gmail.com>
2023-07-24 01:38:08 -0700

    deploying

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,33 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = process.env.JWT_SECRET;
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
   authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    let token = req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-    // if no token passed, return the req
+
     if (!token) {
       return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
+      return res.status(400).json({ message: 'Invalid token!' });
     }
 
-    // return the request object,
-    // which is then passed to the resolver as `context`
     return req;
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-    
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
